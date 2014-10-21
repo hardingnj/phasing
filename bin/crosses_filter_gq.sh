ead1a9c3bf0679ed83beceb34def90e5a6d94fed
Nicholas Harding <njh@foxtrot.well.ox.ac.uk>
2014-10-21 17:33:53 +0100

    Adding script to prepare truth vcf for shapeIt

--- bin/crosses_filter_gq.sh
+++ bin/crosses_filter_gq.sh
@@ -11,15 +11,15 @@ fi
 
 GQ=$1
 
+echo $GQ
 RAW=/data/anopheles/ag-crosses/data/release/0.1.GATK.PHASING.1000AG.AR2/3L_ag-cross.vcf.gz
 OUTDIR=/data/anopheles/ag1000g/data/1000g_09_13/evaluation/phasing/phase1.AR2/callsets/autosome/truth/vcf
 
 OUT="${OUTDIR}/3L_ag-cross.${GQ}"
 
-vcftools --gzvcf ${RAW} --minGQ ${GQ} --recode --out ${OUT}
-bgzip ${OUT}.recode.vcf
-tabix -p vcf ${OUT}.recode.vcf.gz
-touch ${OUT}.recode.vcf.gz.ok
+vcftools --gzvcf ${RAW} --minGQ ${GQ} --chr 3L --from-bp 1 --to-bp 10000 --recode --out ${OUT}
+perl -lne 'print if ($_ =~ m/^#/) || ($_ =~ m:[01]/[012]:)' ${OUT}.recode.vcf | bgzip -c > ${OUT}.vcf.gz
+#rm ${OUT}.recode.vcf
 
-
-
+tabix -p vcf ${OUT}.vcf.gz
+touch ${OUT}.vcf.gz.ok
