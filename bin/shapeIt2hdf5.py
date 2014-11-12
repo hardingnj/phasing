bcefc3af06b112ab05a708b0d5a3411d83ca7581
Nick Harding <njh@well.ox.ac.uk>
2014-11-12 09:35:29 +0000

    Update
    Fixed bug with final chunk size being less
    Uses samples file to get number
    tidied up code
    added flag for compression level

--- bin/shapeIt2hdf5.py
+++ bin/shapeIt2hdf5.py
@@ -17,63 +17,81 @@ parser.add_argument('-c', '--chunksize', dest='chunk_size', action='store',
                     type=int, default=100000,
                     help='Number of lines to read at once from file')
 
+parser.add_argument('-z', '--compression', dest='comp_level', action='store',
+                    type=int, default=1, help='zlib compression level')
+
 parser.add_argument('--chr', dest='chr', action='store',
                     help='contig label to use as key at root of hdf5')
 parser.add_argument('-o', '--out', dest='outfile', action='store',
                     help='path to write hdf5 file')
 args = parser.parse_args()
 
-assert not os.path.isfile(args.outfile)
-h5file = openFile(args.outfile, mode="w")
+if os.path.isfile(args.outfile):
+    raise IOError(args.outfile + ' already exists. Exiting...')
 
-# Get the HDF5 root group
+h5file = openFile(args.outfile, mode="w")
 root = h5file.root
 
 # Create the groups
 chrom = h5file.create_group(root, args.chr)
-
 grp_calldata = h5file.create_group(chrom, "calldata")
 grp_variants = h5file.create_group(chrom, "variants")
+
+# read samples file
+fh_samples = gzip.open(args.samples, 'rb')
+samples_header = fh_samples.readline()
+samples_desc = fh_samples.readline()
+sample_info = fh_samples.readlines()
+sample_names = np.array([s.rstrip().split(' ') for s in sample_info])[:, 1]
+
+# create objects
+filters = Filters(complevel=args.comp_level, complib='zlib')
+samples = h5file.create_array(chrom, 'samples', sample_names)
 
 position = h5file.create_earray(grp_variants, name='POS',
                                 atom=IntAtom(itemsize=4),
                                 expectedrows=1000000, shape=(0, ),
-                                filters=Filters(complevel=9, complib='zlib'))
+                                filters=filters)
 
-identif = h5file.create_earray(grp_variants, name='ID',
-                               atom=StringAtom(itemsize=8),
-                               expectedrows=1000000, shape=(0, ),
-                               filters=Filters(complevel=9, complib='zlib'))
+identify = h5file.create_earray(grp_variants, name='ID',
+                                atom=StringAtom(itemsize=8),
+                                expectedrows=1000000, shape=(0, ),
+                                filters=filters)
 
-ref = h5file.create_earray(grp_variants, name='REF',
-                           atom=StringAtom(itemsize=1),
-                           expectedrows=1000000, shape=(0, ),
-                           filters=Filters(complevel=9, complib='zlib'))
-alt = h5file.create_earray(grp_variants, name='ALT',
-                           atom=StringAtom(itemsize=1),
-                           expectedrows=1000000, shape=(0, ),
-                           filters=Filters(complevel=9, complib='zlib'))
+reference = h5file.create_earray(grp_variants, name='REF',
+                                 atom=StringAtom(itemsize=1),
+                                 expectedrows=1000000, shape=(0, ),
+                                 filters=filters)
 
-f = gzip.open(args.haplotypes, 'rb')
-# look at line to see how many samples...
-n_haps = len(f.readline().split(' ')) - 5
-f.seek(0, 0)  # reset reader
+alternate = h5file.create_earray(grp_variants, name='ALT',
+                                 atom=StringAtom(itemsize=1),
+                                 expectedrows=1000000, shape=(0, ),
+                                 filters=filters)
 
 genotypes = h5file.create_earray(grp_calldata, name='genotype',
                                  atom=IntAtom(itemsize=1),
-                                 expectedrows=1000000, shape=(0, n_haps/2, 2),
-                                 filters=Filters(complevel=9, complib='zlib'))
+                                 expectedrows=1000000,
+                                 shape=(0, sample_names.size, 2),
+                                 filters=filters)
 
+fh_haplotypes = gzip.open(args.haplotypes, 'rb')
 while True:
-    chunk = list(islice(f, args.chunk_size))
+    chunk = list(islice(fh_haplotypes, args.chunk_size))
     if not chunk:
         break
     as_np = np.array([line.rstrip().split(' ') for line in chunk])
-    position.append(as_np[:, 2].astype('int'))
-    identif.append(as_np[:, 1])
-    ref.append(as_np[:, 3])
-    alt.append(as_np[:, 4])
-    genotypes.append(as_np[:, 5:].astype('int').reshape((args.chunk_size,
-                                                         n_haps/2, 2)))
+    try:
+        position.append(as_np[:, 2].astype('int'))
+        identify.append(as_np[:, 1])
+        reference.append(as_np[:, 3])
+        alternate.append(as_np[:, 4])
+
+        geno = as_np[:, 5:].astype('int').reshape((-1, sample_names.size, 2))
+        assert geno.shape[1] == sample_names.size
+        genotypes.append(geno)
+
+    except AssertionError:
+        print sample_names.size, as_np.shape
+        exit(1)
 
 h5file.close()
