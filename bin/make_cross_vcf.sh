1d833480e414a330f41614228fe79740afa65f0a
Nick Harding <njh@well.ox.ac.uk>
2014-11-26 10:34:59 +0000

    Added me filtering step

--- bin/make_cross_vcf.sh
+++ bin/make_cross_vcf.sh
@@ -8,7 +8,13 @@ PHBIN=/home/njh/git/phasing/bin
 RAWH5=/data/anopheles/ag-crosses/data/release/0.1.GATK.PHASING.1000AG.AR2/h5/3L_ag-cross.h5 
 PEDTB=/home/njh/git/ag-crosses/meta/tbl_sample_ped.txt
 MINGQ=40
+
 OUTDI=/data/anopheles/ag1000g/data/1000g_09_13/evaluation/phasing/phase1.AR2/callsets/autosome/truth/
+
+# testing on pc
+#PYBIN=/home/njh/PycharmProjects/phasing/bin
+#OUTDI=/tmp/testing_pipe
+#RAWH5=/home/njh/sync/3L_ag-cross.h5
 
 WORKD=${OUTDI}/gq${MINGQ} 
 FSTEM=3L_ag-cross.gq${MINGQ}
@@ -24,5 +30,14 @@ tabix -fp vcf ${WORKD}/${FSTEM}.vcf.gz
 touch ${WORKD}/${FSTEM}.vcf.gz.ok
 rm ${WORKD}/${FSTEM}.h5
 
+# need to filter by the found mendelian errors
+vcftools --gzvcf ${WORKD}/${FSTEM}.vcf.gz --exclude-positions \
+  ${WORKD}/${FSTEM}_me.txt --recode --out ${WORKD}/${FSTEM}_me
+mv ${WORKD}/${FSTEM}_me.recode.vcf ${WORKD}/${FSTEM}_me.vcf
+bgzip ${WORKD}/${FSTEM}_me.vcf
+tabix -fp vcf ${WORKD}/${FSTEM}_me.vcf.gz
+touch ${WORKD}/${FSTEM}_me.vcf.gz.ok
+
 # this section splits the vcf into the separate cross parts
-$PYENV $PYBIN/split_vcf.py ${WORKD}/${FSTEM}.vcf.gz ${WORKD}/${FSTEM} -P $PEDTB 
+$PYENV $PYBIN/split_vcf.py ${WORKD}/${FSTEM}_me.vcf.gz ${WORKD}/${FSTEM} \
+  -P $PEDTB
