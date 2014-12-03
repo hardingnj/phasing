61750d7ab15d0f3ea540f99e4b16db53be4b18a4
Nick Harding <njh@well.ox.ac.uk>
2014-12-03 15:22:52 +0000

    Now uses the sample file function from utils

--- bin/split_vcf.py
+++ bin/split_vcf.py
@@ -3,23 +3,6 @@ import phasing as ph
 import os
 
 
-def make_sample_file(sample_list, ped_dict, family_id, filepath):
-
-    father, mother = ped_dict['parent']
-    fh = open(filepath, 'w')
-    fh.write('ID_1 ID_2 missing father mother sex plink_pheno' + "\n")
-    fh.write('0 0 0 D D D B' + "\n")
-    for i, s in enumerate(sample_list):
-        if s in ped_dict['parent']:
-            fh.write(" ".join([family_id + '_' + str(i+1), s, '0',
-                               '0', '0', '0', '-9']) + "\n")
-        elif s in ped_dict['progeny']:
-            fh.write(" ".join([family_id + '_' + str(i+1), s, '0',
-                               father, mother, '0', '-9']) + "\n")
-        else:
-            raise Exception('Sample not found in dict.')
-    fh.close()
-
 parser = argparse.ArgumentParser(description='Tool to filter a hdf5 file')
 
 parser.add_argument('input', help='input hdf5 file')
@@ -44,7 +27,7 @@ for k in pedigree.keys():
                     args.output + '_' + k + '.vcf.gz'])
     os.system(cmd)
 
-    make_sample_file(sample_list=samples.split(' '),
-                     ped_dict=pedigree[k],
-                     family_id=k,
-                     filepath=args.output + '_' + k + '.sample')
+    ph.utils.make_sample_file(sample_list=samples.split(' '),
+                              ped_dict=pedigree[k],
+                              family_id=k,
+                              filepath=args.output + '_' + k + '.sample')
