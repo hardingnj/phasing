001c93b2b27654f683da294dee2a8b2f2638254a
Nick Harding <njh@well.ox.ac.uk>
2014-12-18 17:48:29 +0000

    added out option

--- bin/split_vcf.py
+++ bin/split_vcf.py
@@ -6,6 +6,8 @@ import os
 parser = argparse.ArgumentParser(description='Tool to filter a vcf file')
 
 parser.add_argument('input', help='input compressed vcf file')
+parser.add_argument('--out', action='store', dest='out_dir',
+                    default=os.getcwd())
 parser.add_argument('--start', action='store', dest='start', type=int)
 parser.add_argument('--end', action='store', dest='end', type=int)
 
@@ -13,7 +15,8 @@ args = parser.parse_args()
 comment = re.compile('^#')
 
 stem = os.path.split(args.input)[-1].split('.')[0]
-output_fn = "{0}_{1}_{2}.vcf.gz".format(stem, args.start, args.end)
+output_fn = args.out_dir + "/{0}_{1}_{2}.vcf.gz".format(stem,
+                                                        args.start, args.end)
 
 input_fh = gzip.open(args.input, 'r')
 output_fh = gzip.open(output_fn, 'w-')
