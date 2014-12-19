bd9a5e396457188a7129c96c4f5320774db25279
Nick Harding <njh@well.ox.ac.uk>
2014-12-19 10:48:46 +0000

    Fixed issue with { in format string

--- src/phasing/algorithms.py
+++ src/phasing/algorithms.py
@@ -146,14 +146,14 @@ class ShapeIt():
             script_name = os.path.join(self.dirs['script'], str(i) +
                                        '_shapeIt.sh')
             exit_check = "\n".join(["{",
-                                    "if [ -f {0}.ok ]; then",
+                                    "if [ -f %s.ok ]; then",
                                     "\techo \"Already completed ok!\"",
                                     "\texit 0",
                                     "fi",
                                     "}"])
             utils.create_sh_script(
                 filename=script_name,
-                commands=['cd ' + region_dir, exit_check.format(haps),
+                commands=['cd ' + region_dir, exit_check % haps,
                           cmd_shape_it],
                 outfile=haps)
 
