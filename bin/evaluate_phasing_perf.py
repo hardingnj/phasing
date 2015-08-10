abbcd13e4070a805814f0ff9e420d8a77c54b1a4
Nick Harding <njh@well.ox.ac.uk>
2015-08-10 18:45:17 +0100

    updated script to have all plots

--- bin/evaluate_phasing_perf.py
+++ bin/evaluate_phasing_perf.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import yaml
 import pandas as pd
+from intervaltree import IntervalTree, Interval
 import allel
 import bcolz
 import anhima.gt as gt
@@ -25,13 +26,19 @@ from os.path import join
 from math import ceil
 
 
+contig_lengths = {"2L": 49364325, "2R": 61545105,
+                  "3L": 41963435, "3R": 53200684,
+                  "X": 24393108, "Y_unplaced": 237045,
+                  "UNKN": 42389979}
+
+
 # utility function for plot_switch_errors,
 # classifies each position as het good, het bad or non informative.
 def determine_err_locs(positions, evaluated_positions, switches_indexes):
 
     position_switches = np.take(evaluated_positions, switches_indexes)
     result = np.zeros(positions.shape)
-    #print position_switches
+
     for pos in evaluated_positions:
         r = 1
         if pos in position_switches:
@@ -75,30 +82,199 @@ def plot_switch_errors(pos, switch_data, start=None, stop=None):
 
     err_rate[esites < 10] = np.NaN
 
-    fig = plt.figure(figsize=(16, 10))
+    fig = plt.figure(figsize=(12, 10))
 
     # plot genotypes
-    ax = plt.subplot2grid((8, 1), (2, 0), rowspan=6)
+    ax = plt.subplot2grid((8, 1), (0, 0), rowspan=6)
     gt.plot_discrete_calldata(calldata, labels=[s.decode() for s in parent_ids],
                               colors=('grey', 'white', 'red'),
                               states=(0, 1, 2), ax=ax)
 
-    ax = plt.subplot2grid((8, 1), (1, 0), rowspan=1)
-    loc.plot_variant_locator(pos, step=pos.size/100, ax=ax, flip=True)
+    ax = plt.subplot2grid((8, 1), (6, 0), rowspan=1)
+    loc.plot_variant_locator(pos, step=pos.size/100, ax=ax, flip=False)
     ax.xaxis.set_ticklabels([])
 
-    ax = plt.subplot2grid((8, 1), (0, 0), rowspan=1)
+    ax = plt.subplot2grid((8, 1), (7, 0), rowspan=1)
     ax.plot(midpoints, err_rate, 'r')
     ax.set_xlim((start, stop))
     ul = ceil(10 * np.nanmax(err_rate))/10
     ax.set_ylim((0, ul))
     ax.set_yticks(np.arange(0, ul + 0.01, 0.1))
     ax.set_ylabel("Switch error rate")
+    ax.set_xlabel("Genomic position (bp)")
 
-    ax.xaxis.set_major_formatter(
-        ticker.FuncFormatter(lambda y, ps: '%.1f' % (y*1e-6)))
-    fig.subplots_adjust(hspace=0.5)
+    # ax.xaxis.set_major_formatter(
+    #     ticker.FuncFormatter(lambda y, ps: '%.1f' % (y*1e-6)))
+    fig.subplots_adjust(hspace=0.3)
     return fig
+
+
+def calc_marker_dist(pos, homozygosity=None):
+
+    if pos.size == 0:
+        return 0
+    else:
+        start, stop = pos[0], pos[-1]
+
+    if homozygosity is None:
+        sum_roh = 0
+    else:
+        int_tree = IntervalTree(IntervalTree.from_tuples(homozygosity))
+        int_tree.slice(start)
+        int_tree.slice(stop)
+        sum_roh = np.sum([ival.length() for ival in int_tree[start:stop]])
+
+    return stop - start - sum_roh
+
+
+def evaluate_markers(start, markers, error_positions, window_size=2e5):
+    """
+    function that retuns all marker distances, and whether they are a SE.
+    start: is the genomic window start pos
+    markers: an arr of genetic positions of the markers
+    error_pos: an arr of error positions
+    window_size: window size
+
+    """
+    sliced = loc.locate_interval(markers, start, start + window_size)
+    window_markers = markers[sliced]
+    if window_markers.size == 0:
+        return np.empty(0, dtype="int"), np.empty(0, dtype="bool")
+
+    # don't care if first marker is an error...
+    errors = np.in1d(window_markers[1:], error_positions)
+    return window_markers, errors
+
+
+def calculate_switch_distances(start_points, switch_data, window_size, roh_dict):
+
+    # intial declatations
+    mean_marker_dist = np.repeat(np.NaN, start_points.size)
+    mean_switch_dist = np.repeat(np.NaN, start_points.size)
+    conf_switch_dist = np.repeat(np.NaN, 2*start_points.size).reshape((
+        start_points.size, 2))
+
+    marker_count = np.zeros(start_points.size, dtype="int")
+    error_count = np.zeros(start_points.size, dtype="int")
+    pids = switch_data.keys()
+
+    evaluated_pos = {pid: switch_data[pid][2] for pid in pids}
+
+    # pos errors represents the position of switch errors in the chromosome.
+    pos_errors = {pid: np.take(evaluated_pos[pid],
+                               switch_data[pid][1][:-1].cumsum())
+                  for pid in pids}
+
+    for i, start in enumerate(start_points):
+
+        d = {pid: evaluate_markers(start,
+                                   switch_data[pid][2],
+                                   pos_errors[pid],
+                                   window_size) for pid in pids}
+
+        positions = {pid: d[pid][0] for pid in pids}
+        errors = {pid: d[pid][1] for pid in pids}
+
+        distances = [calc_marker_dist(positions[pid],
+                                      roh_dict[pid]) for pid in pids]
+
+        total_distance = np.sum(distances)
+
+        n_markers = np.sum([np.size(p) - 1
+                            for p in positions.values()]).astype("int")
+
+        n_error = np.sum([np.sum(e) for e in errors.values()]).astype("int")
+
+        max_p_err = beta.ppf(0.975, 1 + n_error, 1 + n_markers - n_error)
+        min_p_err = beta.ppf(0.025, 1 + n_error, 1 + n_markers - n_error)
+        p_error = beta.ppf(0.5, 1 + n_error, 1 + n_markers - n_error)
+
+        mean_marker_d = total_distance/n_markers
+        mean_switch_d = mean_marker_d/p_error
+        mean_marker_dist[i] = mean_marker_d
+        mean_switch_dist[i] = mean_switch_d
+        conf_switch_dist[i] = (mean_marker_d/min_p_err, mean_marker_d/max_p_err)
+        marker_count[i] = n_markers
+        error_count[i] = n_error
+        #print(n_error, n_markers, total_distance, mean_marker_d)
+
+    return (mean_marker_dist, mean_switch_dist, conf_switch_dist,
+            marker_count, error_count)
+
+
+def draw_msd_across_genome(genome_pos, marker_dist, mean_switch_dist,
+                           conf_mean_switch_dist, number_markers):
+
+    mean_switch_dist[number_markers < 50] = np.NaN
+    fig = plt.figure(figsize=(12, 3))
+    ax = fig.add_subplot(111)
+
+    ax.plot(genome_pos, 2 * marker_dist, 'k-', linewidth=2.0)
+    ax.plot(genome_pos, mean_switch_dist, 'r-',  linewidth=2.0)
+
+    upper = conf_mean_switch_dist.T[0]
+    lower = conf_mean_switch_dist.T[1]
+
+    cords = np.array([[genome_pos, genome_pos[::-1],
+                       upper, lower[::-1]]]).T.reshape((-1, 2), order="A")
+    poly = plt.Polygon(cords, facecolor="r", edgecolor=None, alpha=0.2)
+
+    ax.add_patch(poly)
+    ax.grid(True)
+    ax.set_ylabel("Mean switch distance (bp)")
+    ax.set_xlabel("Genomic position (bp)")
+    ax.set_yscale("log")
+
+    return fig
+
+
+def draw_switch_err_rate(genome_pos, marker_dist, mean_switch_dist,
+                         conf_mean_switch_dist, number_markers):
+
+    mean_switch_dist[number_markers < 50] = np.NaN
+
+    fig = plt.figure(figsize=(12, 3))
+    ax = fig.add_subplot(111)
+
+    ax.plot(genome_pos, 1 - marker_dist/mean_switch_dist, 'r-', linewidth=2.0)
+    #ax.plot(genome_pos, mean_switch_dist, 'r-',  linewidth=2.0)
+
+    upper = 1 - marker_dist/conf_mean_switch_dist.T[0]
+    lower = 1 - marker_dist/conf_mean_switch_dist.T[1]
+
+    cords = np.array([[genome_pos, genome_pos[::-1],
+                       upper, lower[::-1]]]).T.reshape((-1, 2), order="A")
+    poly = plt.Polygon(cords, facecolor="r", edgecolor=None, alpha=0.2)
+    ax.add_patch(poly)
+
+    ax.grid(True)
+    ax.set_ylabel("1 - (switch error rate)")
+    ax.set_xlabel("Genomic position (bp)")
+
+    return fig
+
+
+def switch_distances_tofile(path, mean_marker_d, mean_switch_d, conf_int, nmark,
+                            nerr, window_starts, window_size):
+
+    columns = "window", "mean marker dist", "mean switch dist", "CI 95%", \
+              "n Errors", "n Markers"
+
+    swindows = ["{0}-{1}".format(int(x), int(y) - 1)
+                for x, y in zip(window_starts, window_starts + window_size)]
+
+    ci = ["{0},{1}".format(np.round(x, 2), np.round(y, 2))
+          for y, x in conf_int]
+
+    frame = pd.DataFrame(columns=columns)
+    frame[columns[0]] = swindows
+    frame[columns[1]] = np.round(mean_marker_d, 2)
+    frame[columns[2]] = np.round(mean_switch_d, 2)
+    frame[columns[3]] = ci
+    frame[columns[4]] = nerr
+    frame[columns[5]] = nmark
+    frame.to_csv(path + ".txt", sep="\t", index=False)
+    frame.to_latex(path + ".tex", index=False)
 
 
 parser = argparse.ArgumentParser(
@@ -151,6 +327,7 @@ eval_fh = h5py.File(args.eval, "r")
 eval_samples = eval_fh[args.chrom]["samples"][:].tolist()
 test_samples = test_fh[args.chrom]['samples'][:].tolist()
 
+
 if args.samples is not None:
     test_idx = [test_samples.index(s) for s in args.samples]
     eval_idx = [eval_samples.index(s) for s in args.samples]
@@ -160,6 +337,18 @@ else:
     test_idx = [test_samples.index(s) for s in intersection]
     eval_idx = [eval_samples.index(s) for s in intersection]
     sample_names = intersection
+
+if args.roh is not None:
+    roh_fh = h5py.File(args.roh, "r")
+    roh = dict()
+    for s in sample_names:
+        try:
+            roh[s] = roh_fh[args.chrom][s][:]
+        except KeyError:
+            roh[s] = None
+else:
+    roh = {s: None for s in sample_names}
+
 
 t_geno = allel.GenotypeCArray.from_hdf5(test_fh[args.chrom]["calldata"][
     "genotype"])
@@ -200,11 +389,19 @@ for idx, sid in enumerate(sample_names):
 
 het_positions = gt.is_het(e_gt).any(axis=1)
 pl = plot_switch_errors(np.compress(het_positions, e_pos), data)
-pl.savefig(filestem + "switch_errors.png")
+pl.savefig(filestem + "switch_errors.png", bbox_inches="tight")
 
 # now build the windowed MSD array.
-# ignore ROH for now....
-#windows = np.arange(1, contig_lengths[args.chrom], args.winsize).astype('int')
+windows = np.arange(1, contig_lengths[args.chrom], args.winsize).astype('int')
 
-# mmd, msd, conf, nmarker, nerror = calculate_switch_distances(
-#     windows, data, window_size=5e5, roh=None)
+mmd, msd, conf, nmarker, nerror = calculate_switch_distances(
+    windows, data, window_size=args.overlap, roh_dict=roh)
+
+pl = draw_msd_across_genome(windows, mmd, msd, conf, nmarker)
+pl.savefig(filestem + "mean_switch_distance.png", bbox_inches="tight")
+
+pl = draw_switch_err_rate(windows, mmd, msd, conf, nmarker)
+pl.savefig(filestem + "switch_error_rate.png", bbox_inches="tight")
+
+switch_distances_tofile(filestem + "table_switch_errors", mmd, msd, conf,
+                        nmarker, nerror, windows, args.overlap)
