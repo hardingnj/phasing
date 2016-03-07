f74ccff0066d8691027d5634f068b571c4ba71ac
Nick Harding <njh@well.ox.ac.uk>
2016-03-07 18:11:40 +0000

    reverted

--- src/phasing/__init__.py
+++ src/phasing/__init__.py
@@ -1,1 +1,13 @@
-[]
+"""
+The ``phasing`` package.
+
+"""
+
+from __future__ import division, print_function, unicode_literals, \
+    absolute_import
+
+__version__ = '0.5.0'
+
+from . import utils
+from . import switch
+from . import switch_plots
